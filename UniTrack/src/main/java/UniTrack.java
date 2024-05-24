0d2e98e8bd184a58cc2b7e4d9e63bb27fac8d303
Krichet1084 <krishaysharma22@gmail.com>
2024-05-23 23:26:39 -0400

    chatgpt enlightened me into properly using the google classroom api

---
+++ UniTrack/src/main/java/UniTrack.java
@@ -0,0 +1,25 @@
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author krish
+ */
+public class UniTrack {
+
+    public static void main(String[] args) throws IOException, GeneralSecurityException {
+        System.out.println("Creating user");
+        createUser();
+    }
+    
+    public static void createUser() throws IOException, GeneralSecurityException{
+        User test = new User();
+    }
+}
