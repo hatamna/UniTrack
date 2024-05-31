9b2b82fb394cf8481dcec082900a3665b668fc87
Krichet <101164213+Krichet1084@users.noreply.github.com>
2024-05-31 13:14:55 -0400

    surely it opens the link properly now right?¿°¿°

--- UniTrack/src/main/java/User.java
+++ UniTrack/src/main/java/User.java
@@ -9,6 +9,7 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Desktop;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
